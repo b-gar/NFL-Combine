98b03f136c45e9a69e015929a9aea609613a81e7
Ben Garski <blgarski@uwm.edu>
2020-05-03 19:07:39 -0500

    10-Years of NFL Data

---
+++ combine.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 18:02:30 2020
+
+@author: Owner
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+%matplotlib inline
+
+colname = ['Player', 'Position', 'School', 'College', 'Height', 'Weight', '40Yard', 'Vertical', 'Bench', 'Broad', '3Cone', 'Shuttle', 'Drafted']
+
+combine10 = pd.read_csv("combine2010.csv", names=colname, header = 0)
+combine11 = pd.read_csv("combine2011.csv", names=colname, header = 0)
+combine12 = pd.read_csv("combine2012.csv", names=colname, header = 0)
+combine13 = pd.read_csv("combine2013.csv", names=colname, header = 0)
+combine14 = pd.read_csv("combine2014.csv", names=colname, header = 0)
+combine15 = pd.read_csv("combine2015.csv", names=colname, header = 0)
+combine16 = pd.read_csv("combine2016.csv", names=colname, header = 0)
+combine17 = pd.read_csv("combine2017.csv", names=colname, header = 0)
+combine18 = pd.read_csv("combine2018.csv", names=colname, header = 0)
+combine19 = pd.read_csv("combine2019.csv", names=colname, header = 0)
+combine20 = pd.read_csv("combine2020.csv", names=colname, header = 0)
+
+df = pd.concat([combine12,combine13,combine14,combine15,combine16,combine17,combine18,combine18,combine20])
+del df['College']
+
+# Get Players Name
+df[['Name', 'ID']] = df.Player.apply(lambda x: pd.Series(str(x).split("\\")))
+del df['Player']
+
+# Convert Height to Number
+df[['Feet','Inches']] = df['Height'].str.split('-', 1, expand=True)
+df.Feet = df.Feet.astype(int)
+df.Inches = df.Inches.astype(int)
+df['Height'] = (12 * df['Feet']) + df['Inches']
+
+# Turn Drafted Binary
+df['Drafted'] = df['Drafted'].notnull().astype(int)
+
+# One Hot Encode
+dfohe = pd.concat([pd.get_dummies(df['Position']), df], axis = 1)
+dfohe = dfohe.drop(['Feet', 'Inches', 'Position', 'ID'], axis = 1)
+
+# Normalize
+normCols = ['Height', 'Weight', '40Yard', 'Vertical', 'Bench', 'Broad', '3Cone', 'Shuttle']
+dfnorm = df[normCols]
+dfnorm=(dfnorm-dfnorm.mean())/dfnorm.std()
+
+# Get Final DF and Drop NA's
+dfclean = pd.concat([dfohe.drop(normCols, axis=1), dfnorm], axis = 1)
+dfclean = dfclean.dropna()
+
+# Get Features
+ints1 = np.arange(0,25,1)
+ints2 = np.arange(28,36,1)
+ints = np.concatenate((ints1,ints2), axis=0).tolist()
+
+x = dfclean.iloc[:,ints]
+X = x.values
+
+# Get Target
+y = dfclean.Drafted
+Y = y.values
+
+# Initialized Methods
+logreg = LogisticRegression()
+rf = RandomForestClassifier()
+
+kf = KFold(n_splits=10)
+for k, (train, test) in enumerate(kf.split(X, Y)):
+    
+    # Logistic Regression
+    print("===== Logistic Regression =====")
+    logreg.fit(X[train], Y[train])
+    logpred = logreg.predict(X[test])
+    logcm = confusion_matrix(Y[test], logpred)
+    print("Accuracy:", metrics.accuracy_score(Y[test], logpred))
+    
+    # Random Forest
+    print("===== Random Forest =====")
+    rf.fit(X[train], Y[train])
+    rfpred = rf.predict(X[test])
+    rfcm = confusion_matrix(Y[test], rfpred)
+    print("Accuracy:", metrics.accuracy_score(Y[test], rfpred))
+    
